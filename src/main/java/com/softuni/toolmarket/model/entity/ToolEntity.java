102c32b56dad7f90a4077809961ebee014df25cf
VasilSV <114056331+VasilSV@users.noreply.github.com>
2023-03-27 14:29:29 +0300

    project

---
+++ src/main/java/com/softuni/toolmarket/model/entity/ToolEntity.java
@@ -0,0 +1,67 @@
+package com.softuni.toolmarket.model.entity;
+
+import jakarta.persistence.*;
+
+@Table(name = "tools")
+@Entity
+public class ToolEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Column(nullable = false)
+    private String toolName;
+    @Column(columnDefinition = "text")
+    private String description;
+
+    @ManyToOne
+    private UserEntity userName;
+
+    public ToolEntity() {
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public ToolEntity setId(Long id) {
+        Id = id;
+        return this;
+    }
+
+    public String getToolName() {
+        return toolName;
+    }
+
+    public ToolEntity setToolName(String toolName) {
+        this.toolName = toolName;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public ToolEntity setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public UserEntity getUserName() {
+        return userName;
+    }
+
+    public ToolEntity setUserName(UserEntity userName) {
+        this.userName = userName;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "ToolEntity{" +
+                "Id=" + Id +
+                ", toolName='" + toolName + '\'' +
+                ", description='" + description + '\'' +
+                ", userName=" + userName +
+                '}';
+    }
+}
---
+++ src/main/resources/static/js/medialko.js
@@ -0,0 +1,3 @@
+function medialko(){
+   // return false
+}
