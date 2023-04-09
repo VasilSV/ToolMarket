d329e821cd99d48bacca70dda4634d6ff200ac0d
VasilSV <114056331+VasilSV@users.noreply.github.com>
2023-04-10 01:21:55 +0300

    project

---
+++ src/main/java/com/softuni/toolmarket/ToolMarketApplication.java
@@ -0,0 +1,13 @@
+package com.softuni.toolmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolMarketApplication.class, args);
+	}
+
+}
