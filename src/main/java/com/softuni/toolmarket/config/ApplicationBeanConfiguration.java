9d968d09556ce1e04ce9847cb8c95f5d86f5776f
VasilSV <114056331+VasilSV@users.noreply.github.com>
2023-10-30 10:31:23 +0200

    project

--- src/main/java/com/softuni/toolmarket/config/ApplicationBeanConfiguration.java
+++ src/main/java/com/softuni/toolmarket/config/ApplicationBeanConfiguration.java
@@ -1,7 +1,6 @@
 package com.softuni.toolmarket.config;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
@@ -13,13 +12,7 @@ import java.time.format.DateTimeFormatter;
 @Configuration
 public class ApplicationBeanConfiguration {
 
-    @Bean
-    public Gson gson() {
-        return new GsonBuilder()
-                .excludeFieldsWithoutExposeAnnotation()
-                .setPrettyPrinting()
-                .create();
-    }
+
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
